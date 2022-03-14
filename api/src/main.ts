427c23671e0ddc6665c017621e3ed2cb3c05485b
xrexy <rexydoesimportantthings@gmail.com>
2022-03-14 19:03:44 +0200

    Added api version

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
