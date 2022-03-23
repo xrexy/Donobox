07e58aa69bdb3e0e0c2ea6ad3b7d3ce884aac6cf
xrexy <rexydoesimportantthings@gmail.com>
2022-03-23 17:44:48 +0200

    Added fundraiser graphql resolver

--- api/src/fundraisers/fundraisers.resolver.ts
+++ api/src/fundraisers/fundraisers.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Fundraiser } from 'src/utils/graphql/models/fundraiser.model';
+import { GetFundraiserArgs } from './dto/args/get-fundraiser.args';
+import { FundraisersService } from './fundraisers.service';
 
 @Resolver()
-export class FundraisersResolver {}
+export class FundraisersResolver {
+  constructor(private readonly fundraisersService: FundraisersService) {}
+
+  @Query(() => Fundraiser, { name: 'fundraiser', nullable: true })
+  async getDonation(@Args() getFundraiserArgs: GetFundraiserArgs) {
+    return getFundraiserArgs;
+  }
+}
