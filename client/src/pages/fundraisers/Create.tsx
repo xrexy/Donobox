d5bdffb648792b61e67ea6ee5de9cb3fbb05e520
xrexy <rexydoesimportantthings@gmail.com>
2022-03-23 17:51:38 +0200

    Added RichTextEditor

--- client/src/pages/fundraisers/Create.tsx
+++ client/src/pages/fundraisers/Create.tsx
@@ -1,9 +1,20 @@
+import RichTextEditor from '@mantine/rte';
 import React from 'react';
 import { Shell } from '../../components/Shell';
 
 interface Props {}
 
-export const CreateFundraiserPage: React.FC<Props> = () => {
-  const a = 2;
-  return <Shell>{a}</Shell>;
-};
+export const CreateFundraiserPage: React.FC<Props> = () => (
+  <Shell>
+    <RichTextEditor
+      controls={[
+        ['bold', 'italic', 'underline', 'link', 'image'],
+        ['unorderedList', 'h1', 'h2', 'h3'],
+        ['sup', 'sub'],
+        ['alignLeft', 'alignCenter', 'alignRight'],
+      ]}
+      value=""
+      onChange={() => {}}
+    />
+  </Shell>
+);
